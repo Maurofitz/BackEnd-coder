e598b2436f0d51577eaf7fa18c716c6946c730f6
Mauro Fitz Maurice <maurofitzmaurice@gmail.com>
2022-08-23 21:49:24 -0300

    agreglos apiresful

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const index = express.Router();
+const routerProductos = require('./routes/productos,js');
 
-index.get('/', (req,res) => {
-    res.render('index');
-})
+const PORT = 8080;
+const app = express();
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.on('error', err => console.log(`Error: ${err}`));
 
-module.exports = index;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api', routerProductos);
